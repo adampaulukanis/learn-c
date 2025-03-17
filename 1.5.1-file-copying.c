7160c79a505ee04cb511b9880574b7336e41b2d8
Adam Paulukanis <adam.paulukanis@gmail.com>
2025-03-17 23:41:53 +0000

    1.5.1: file-copying version 1st

---
+++ 1.5.1-file-copying.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * Copy input to output.
+ * 1st version.
+ */
+int main(void)
+{
+    int c;
+
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+
+    return 0;
+}
