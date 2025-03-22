f44d2ef75e39023e4587c7f49467067935ba5403
Adam Paulukanis <adam.paulukanis@gmail.com>
2025-03-22 00:14:37 +0000

    1.5.2 Character Counting v2
    Use for loop instead of while and use double to keep count of the number
    of characters.

--- 1.5.2-character-counting.c
+++ 1.5.2-character-counting.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /*
- * Count characters in input; 1st version
+ * Count characters in input; 2nd version
  */
 int main(void)
 {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 
     return 0;
 }
