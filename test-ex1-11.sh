5fc3209bb3da63076325520e9d7cf85218b1ff2d
Adam Paulukanis <adam.paulukanis@gmail.com>
2025-04-11 11:17:00 +0100

    ex1-11: update
    I added a temp file and the content of this file is generated by
    openssl. It is a "normal" file (ASCII), not binary.

--- test-ex1-11.sh
+++ test-ex1-11.sh
@@ -4,31 +4,35 @@
 . ./assert.sh
 
 # clean and make the program
-rm 1.5.4-word-counting
+rm -f 1.5.4-word-counting
 make 1.5.4-word-counting
 
+# preparations
+TMPFILE=`mktemp /tmp/test.XXXXXXXXXXXXXXXXXX` || exit 1
+/usr/bin/openssl rand -base64 4321 -out $TMPFILE
+
 # main
-set +A lineswordschars $(cat *.c |./1.5.4-word-counting)
+set +A lineswordschars $(cat $TMPFILE |./1.5.4-word-counting)
 
 # define tests
 # tr " " "\0" replaces spaces with nothing
 function test_lines {
     actual=${lineswordschars[0]}
-    expected=$(cat *.c |wc -l |tr " " "\0")
+    expected=$(cat $TMPFILE |wc -l |tr " " "\0")
     assert_eq "$actual" "$expected"
     [ "$?" == 0 ] && log_success "$0" || log_failure "$0"
 }
 
 function test_words {
     actual=${lineswordschars[1]}
-    expected=$(cat *.c |wc -w |tr " " "\0")
+    expected=$(cat $TMPFILE |wc -w |tr " " "\0")
     assert_eq "$actual" "$expected"
     [ "$?" == 0 ] && log_success "$0" || log_failure "$0"
 }
 
 function test_chars {
     actual=${lineswordschars[2]}
-    expected=$(cat *.c |wc -c |tr " " "\0")
+    expected=$(cat $TMPFILE |wc -c |tr " " "\0")
     assert_eq "$actual" "$expected"
     [ "$?" == 0 ] && log_success "$0" || log_failure "$0"
 }
